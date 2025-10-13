ccb6e6a5b444326ce360e7702fb4089d5fbfd9c0
T3DStudio <t3dstd@gmail.com>
2025-10-13 14:54:28 +0300

    v2

--- kolyan.py
+++ kolyan.py
@@ -10,31 +10,34 @@ def init_config(config_path : str):
     config = configparser.ConfigParser()
     config.read(config_path)
     botToken = config['DEFAULT']['BotToken']
-    shitRate = config['DEFAULT']['ShitRate']
     kolyanId = config['DEFAULT']['KolyanId']
     DBPath = config['DEFAULT']['DBPath']
-    return botToken, float(shitRate), int(kolyanId), DBPath
+    return botToken, int(kolyanId), DBPath
 
-botToken, shitRate, kolyanId, DBPath = init_config(defaultConfigPath)
+botToken, kolyanId, DBPath = init_config(defaultConfigPath)
 
 db_connection = sqlite3.connect(DBPath, check_same_thread=False, isolation_level=None)
 lock = threading.Lock()
 
 cursor = db_connection.cursor()
 
-def init_db():
-    cursor.execute('CREATE TABLE IF NOT EXISTS reply_messages(submitted_by bigint NOT NULL, message_text text NOT NULL UNIQUE)')
-    cursor.execute('CREATE TABLE IF NOT EXISTS admins(id bigint NOT NULL UNIQUE)')
-
-
-def check_admin(id : int):
-    cursor.execute('SELECT id FROM admins WHERE id = ?', [id])
-    if cursor and list(cursor):
-        return True
-    return False
-
-
 kolyabot = telebot.TeleBot(botToken)
+
+########################################    INIT
+
+def init_db():
+    with lock:
+        cursor.execute('CREATE TABLE IF NOT EXISTS reply_messages(submitted_by bigint NOT NULL, message_text text NOT NULL UNIQUE)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS admins(id bigint NOT NULL UNIQUE)')
+
+########################################    COMMON
+        
+def check_admin(id : int):
+    with lock:
+        cursor.execute('SELECT id FROM admins WHERE id = ?', [id])
+        if cursor and list(cursor):
+            return True
+    return False
 
 def check_reply(message : telebot.types.Message):
     user_id = message.from_user.id
@@ -46,13 +49,66 @@ def check_reply(message : telebot.types.Message):
         return True
     return False
 
+def random_msg(message : telebot.types.Message):
+    with lock:
+         cursor.execute("SELECT message_text FROM reply_messages ORDER BY RANDOM() LIMIT 1")
+         rep_list = list(cursor)
+         if len(rep_list) == 0:
+             randomReply = 'ЭУЪ'
+         else:
+             randomReply = rep_list[0]
+    kolyabot.reply_to(message, text=randomReply)
+
+########################################    HANDLERS
 
 @kolyabot.message_handler(func=check_reply, content_types=['audio', 'photo', 'voice', 'video', 'document',
-                                                                    'text', 'location', 'contact', 'sticker'])
+                                                            'text', 'location', 'contact', 'sticker'])
 def reply_message(message : telebot.types.Message):
-    cursor.execute("SELECT message_text FROM reply_messages ORDER BY RANDOM() LIMIT 1")
-    randomReply = list(cursor)[0]
-    if random.random() > shitRate:
+    if not message.text:
+        return
+    ltext = message.text.lower()
+    ########## BAZA
+    if 'каля' in ltext:
+        random_msg(message)
+    ########## REPLAY-TO-REPLAY-TO-BOT
+    elif message.reply_to_message is not None and message.reply_to_message.from_user.id == kolyabot.get_me().id:
+        random_msg(message)
+    ########## TEST
+    elif ltext.startswith('/test'):
+        random_msg(message)
+    ########## ADD
+    elif ltext.startswith('/add'):
+        if check_admin(message.from_user.id):
+            user_id = message.from_user.id
+            text = message.text[4:].strip()
+            try:
+                with lock:
+                    cursor.execute('INSERT INTO reply_messages(submitted_by, message_text) VALUES (?, ?)', [user_id, text])
+            except sqlite3.IntegrityError as e:
+                kolyabot.reply_to(message, 'Не добавил ебать ты че')
+            else:
+                kolyabot.reply_to(message, 'Базар жок')
+    ########## GRANT
+    elif ltext.startswith('/grant'):
+        print('request for admin:',message.from_user.full_name)
+        user_id = message.from_user.id
+        if 'POTNOFMOB' in message.text:
+            print('granted ',user_id)
+            try:
+                with lock:
+                    cursor.execute('INSERT INTO admins(id) VALUES (?)', [user_id])
+            except sqlite3.IntegrityError as e:
+                print('already added ',user_id)
+            kolyabot.reply_to(message, text='ДОБАВИЛ ЖОК')
+        else:
+            print('refused ',user_id)
+            kolyabot.reply_to(message, text='ТЫ КТО ВООБЩЕ ЭУ')
+
+'''
+@kolyabot.message_handler(commands=['test'])
+def add_reply(message : telebot.types.Message):
+    if check_admin(message.from_user.id):
+        randomReply = random_msg()
         kolyabot.reply_to(message, text=randomReply)
 
 @kolyabot.message_handler(commands=['add'])
@@ -61,7 +117,8 @@ def add_reply(message : telebot.types.Message):
         user_id = message.from_user.id
         text = message.text[4:].strip()
         try:
-            cursor.execute('INSERT INTO reply_messages(submitted_by, message_text) VALUES (?, ?)', [user_id, text])
+            with lock:
+                cursor.execute('INSERT INTO reply_messages(submitted_by, message_text) VALUES (?, ?)', [user_id, text])
         except sqlite3.IntegrityError as e:
             kolyabot.reply_to(message, 'Не добавил ебать ты че')
         else:
@@ -69,12 +126,31 @@ def add_reply(message : telebot.types.Message):
 
 @kolyabot.message_handler(commands=['grant'])
 def add_admin(message : telebot.types.Message):
-    print(message.from_user.full_name)
+    print('request for admin:',message.from_user.full_name)
+    user_id = message.from_user.id
     if 'POTNOFMOB' in message.text:
-        print('huj')
-        user_id = message.from_user.id
-        cursor.execute('INSERT INTO admins(id) VALUES (?)', [user_id])
-
+        print('granted ',user_id)
+        try:
+            with lock:
+                cursor.execute('INSERT INTO admins(id) VALUES (?)', [user_id])
+        except sqlite3.IntegrityError as e:
+            print('already added ',user_id)
+        kolyabot.reply_to(message, text='ДОБАВИЛ ЖОК')
+    else:
+        print('refused ',user_id)
+        kolyabot.reply_to(message, text='ТЫ КТО ВООБЩЕ ЭУ')
+'''
+'''
+@kolyabot.message_handler(commands=['showall'])
+def show_all(message : telebot.types.Message):
+    if check_admin(message.from_user.id):
+        mlist = []
+        with lock:
+            cursor.execute("SELECT message_text FROM reply_messages")
+            for msg in list(cursor):
+                mlist.append(msg[0])
+        kolyabot.reply_to(message, "\n".join(mlist))
+'''
 
 def main():
     init_db()
